b07f772bb5bbd243fb8a23937d5c2e109a3a1882
MisKuchum <61450683+MisKuchum@users.noreply.github.com>
2024-02-01 22:32:22 +0300

    L4 Homework added.

--- app/src/main/java/com/example/androidtasks/ActivityA.kt
+++ app/src/main/java/com/example/androidtasks/ActivityA.kt
@@ -2,6 +2,7 @@ package com.example.androidtasks
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,9 +16,43 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidtasks.ui.theme.AndroidTasksTheme
 
 class ActivityA : ComponentActivity() {
+    private val TAG = "MyApp"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "ActivityA is [onCreate] now")
+
         setContentView(R.layout.activity_a)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "ActivityA is [onStart] now")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "ActivityA is [onRestart] now")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "ActivityA is [onResume] now")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "ActivityA is [onPause] now")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "ActivityA is [onStop] now")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "ActivityA is [onDestroy] now")
     }
 
     fun onClickOpenActivityB(view: View) {
--- app/src/main/java/com/example/androidtasks/ActivityB.kt
+++ app/src/main/java/com/example/androidtasks/ActivityB.kt
@@ -4,15 +4,49 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 
 class ActivityB : AppCompatActivity() {
+    private val TAG = "MyApp"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "ActivityB is [onCreate] now")
+
         setContentView(R.layout.activity_b)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "ActivityB is [onStart] now")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "ActivityB is [onRestart] now")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "ActivityB is [onResume] now")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "ActivityB is [onPause] now")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "ActivityB is [onStop] now")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "ActivityB is [onDestroy] now")
     }
 
     fun onClickOpenActivityC(view: View) {
