eaeeebb89924f1ee5b51d9a8cbc3bf392531fea7
MisKuchum <61450683+MisKuchum@users.noreply.github.com>
2024-02-03 23:26:56 +0300

    L5 Homework added.

--- app/src/main/java/com/example/androidtasks/ActivityB.kt
+++ app/src/main/java/com/example/androidtasks/ActivityB.kt
@@ -1,12 +1,10 @@
 package com.example.androidtasks
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 
