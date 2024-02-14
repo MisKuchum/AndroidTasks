175225a4ee126793fb661179942efc53c8217901
MisKuchum <61450683+MisKuchum@users.noreply.github.com>
2024-02-14 17:03:23 +0300

    L9 HomeWork added.
    L9 HomeWork added.

--- app/src/main/java/com/example/androidtasks/ActivityB.kt
+++ app/src/main/java/com/example/androidtasks/ActivityB.kt
@@ -4,19 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 
 class ActivityB : AppCompatActivity() {
     private val TAG = "MyApp"
     private lateinit var actionCTextView: TextView
+    private lateinit var greenTextView: TextView
+    private lateinit var redTextView: TextView
+    private lateinit var blueTextView: TextView
+    private lateinit var orangeTextView: TextView
+    private lateinit var purpleTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "ActivityB is [onCreate] now")
 
         setContentView(R.layout.activity_b)
+        initViews()
+        setTvOnClickListeners()
         actionCTextView = findViewById(R.id.tw_action_c_text)
     }
 
@@ -50,7 +57,7 @@ class ActivityB : AppCompatActivity() {
         Log.d(TAG, "ActivityB is [onDestroy] now")
     }
 
-    fun onClickOpenActivityC(view: View) {
+    fun onClickOpenActivityC() {
         val intent = Intent(this, ActivityC::class.java)
         getResult.launch(intent)
     }
@@ -64,4 +71,30 @@ class ActivityB : AppCompatActivity() {
                 actionCTextView.text = "Input from ActivityC: $value"
             }
         }
+
+    private fun initViews() {
+        actionCTextView = findViewById(R.id.tw_action_c_text)
+        greenTextView = findViewById(R.id.tv_green)
+        redTextView = findViewById(R.id.tv_red)
+        blueTextView = findViewById(R.id.tv_blue)
+        orangeTextView = findViewById(R.id.tv_orange)
+        purpleTextView = findViewById(R.id.tv_purple)
+    }
+
+    private fun setTvOnClickListeners() {
+        val colorTvList = listOf(
+            greenTextView,
+            redTextView,
+            orangeTextView,
+            blueTextView,
+            purpleTextView
+        )
+
+        colorTvList.forEach { tv ->
+            tv.setOnClickListener {
+                Toast.makeText(this, tv.tag.toString(), Toast.LENGTH_SHORT).show()
+                Log.d(TAG, tv.tag.toString())
+            }
+        }
+    }
 }
