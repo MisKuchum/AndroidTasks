cb660510e9ac02561e0f4404d6947fb526eb86f7
MisKuchum <61450683+MisKuchum@users.noreply.github.com>
2024-03-05 21:44:52 +0300

    L24. Added ViewPager on WorkFragment.

--- app/src/main/java/com/example/androidtasks/WorkFragment.kt
+++ app/src/main/java/com/example/androidtasks/WorkFragment.kt
@@ -5,13 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 
 class WorkFragment : Fragment() {
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_work, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewPager = view.findViewById(R.id.view_pager)
+
+        val images = listOf(
+            R.drawable.work,
+            R.drawable.work2,
+            R.drawable.work3,
+            R.drawable.work4
+        )
+        val adapter = ViewPagerAdapter(images)
+        viewPager.adapter = adapter
     }
 
     companion object {
