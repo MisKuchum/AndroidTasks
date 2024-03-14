bcfc4c53de9bf0d64bfcf8779cda35787f61a7b4
MisKuchum <61450683+MisKuchum@users.noreply.github.com>
2024-03-14 19:41:53 +0300

    L23. Added BottomNavigationView on ActivityC.
    Добавил BottomNavigationView на ActivityC. Перенёс почти всю логику, которая была на активности во фрагмент ProfileFragment и добавил два новых - WorkFragment и HobbyFragment.

---
+++ app/src/main/java/com/example/androidtasks/HobbyFragment.kt
@@ -0,0 +1,27 @@
+package com.example.androidtasks
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.VideoView
+
+class HobbyFragment : Fragment() {
+    private lateinit var videoView: VideoView
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hobby, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        videoView = view.findViewById(R.id.videoView)
+        videoView.setVideoURI(Uri.parse("android.resource://" + context?.packageName + "/" + R.raw.hobby))
+        videoView.start()
+    }
+}
--- app/src/main/java/com/example/androidtasks/ProfileFragment.kt
+++ app/src/main/java/com/example/androidtasks/ProfileFragment.kt
@@ -1,60 +1,56 @@
 package com.example.androidtasks
 
+import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
-import android.app.Activity
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import android.Manifest
-import android.location.Geocoder
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import java.util.Locale
-import android.widget.ImageView
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
-class ActivityC : AppCompatActivity() {
-    private val TAG = "MyApp"
+class ProfileFragment : Fragment() {
     private var hasLocationPermissions = false
+    private val TAG = "MyApp"
+
     private lateinit var etBirthdate: EditText
     private lateinit var spBenchPress: Spinner
     private lateinit var etCity: EditText
     private lateinit var btnGetLocation: Button
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var ivProfilePhoto: ImageView
-    private lateinit var bottomNavigationView: BottomNavigationView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "ActivityC is [onCreate] now")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
 
-        setContentView(R.layout.activity_c)
-        etBirthdate = findViewById(R.id.et_birthdate)
-        spBenchPress = findViewById(R.id.sp_bench_press)
-        etCity = findViewById(R.id.et_city)
-        btnGetLocation = findViewById(R.id.btn_get_location)
-        ivProfilePhoto = findViewById(R.id.iv_profile_photo)
-        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        etBirthdate = view.findViewById<EditText>(R.id.et_birthdate)
+        spBenchPress = view.findViewById(R.id.sp_bench_press)
+        etCity = view.findViewById(R.id.et_city)
+        btnGetLocation = view.findViewById(R.id.btn_get_location)
 
         val difficultyArray = resources.getStringArray(R.array.difficulty)
         val spDifficultyAdapter = ArrayAdapter(
-            this,
+            requireContext(),
             androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
             difficultyArray)
         spBenchPress.adapter = spDifficultyAdapter
@@ -73,22 +69,15 @@ class ActivityC : AppCompatActivity() {
                     else -> "Инопланетянин какой-то"
                 }
 
-                Toast.makeText(this@ActivityC, power, Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), power, Toast.LENGTH_SHORT).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
 
-        ivProfilePhoto.setOnClickListener {
-            Intent(Intent.ACTION_GET_CONTENT).also {
-                it.type = "image/*"
-                startActivityForResult(it, 0)
-            }
-        }
-
         firstCheckPermissions()
 
-        val setLocationDialog = AlertDialog.Builder(this)
+        val setLocationDialog = AlertDialog.Builder(requireContext())
             .setTitle("Установка местоположения")
             .setMessage("Вы хотите установить местоположение автоматически?")
             .setIcon(R.drawable.ic_add_location)
@@ -96,62 +85,13 @@ class ActivityC : AppCompatActivity() {
                 setLocation()
             }
             .setNegativeButton("Нет") {_, _ ->
-                Toast.makeText(this, "Тогда зачем кнопку нажимал?", Toast.LENGTH_LONG).show()
+                Toast.makeText(requireContext(), "Тогда зачем кнопку нажимал?", Toast.LENGTH_LONG).show()
             }
             .create()
-
-        setCurrentFragment(ProfileFragment())
-        bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.mi_profile  -> setCurrentFragment(ProfileFragment())
-                R.id.mi_work -> setCurrentFragment(WorkFragment())
-                R.id.mi_hobby -> setCurrentFragment(HobbyFragment())
-            }
-            true
-        }
 
         btnGetLocation.setOnClickListener {
             setLocationDialog.show()
         }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if(resultCode == Activity.RESULT_OK && requestCode == 0) {
-            val uri = data?.data
-            ivProfilePhoto.setImageURI(uri)
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "ActivityC is [onStart] now")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "ActivityC is [onRestart] now")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "ActivityC is [onResume] now")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "ActivityC is [onPause] now")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "ActivityC is [onStop] now")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "ActivityC is [onDestroy] now")
     }
 
     override fun onRequestPermissionsResult(
@@ -168,25 +108,16 @@ class ActivityC : AppCompatActivity() {
                 setLocation()
             }
             else
-                Toast.makeText(applicationContext, "Нужно разрешение", Toast.LENGTH_LONG)
+                Toast.makeText(requireContext(), "Нужно разрешение", Toast.LENGTH_LONG)
                     .show()
         }
     }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean = MyOptionsMenu().create(this, menu)
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean = MyOptionsMenu().itemSelected(this, item)
-
-    fun onClickInputButton(view: View) {
-        intent.putExtra("input", etBirthdate.text.toString())
-        setResult(RESULT_OK, intent)
-        finish()
-    }
 
     private fun hasAccessCoarseLocationPermission() =
-        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
     private fun hasAccessFineLocationPermission() =
-        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
     private fun requestPermissions() {
         var permissionsToRequest = mutableListOf<String>()
@@ -199,19 +130,19 @@ class ActivityC : AppCompatActivity() {
         Log.d(TAG, "${permissionsToRequest.toString()}")
 
         if (permissionsToRequest.isNotEmpty())
-            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), 0)
+            requestPermissions(permissionsToRequest.toTypedArray(), 0)
         else
             hasLocationPermissions = true
     }
 
     private fun setLocation() {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
 
         if (hasLocationPermissions) {
             fusedLocationClient.lastLocation
                 .addOnSuccessListener { location : Location? ->
                     if (location != null) {
-                        val geoCoder = Geocoder(this, Locale.getDefault())
+                        val geoCoder = Geocoder(requireContext(), Locale.getDefault())
                         val address = geoCoder.getFromLocation(location.latitude,location.longitude,1)
                         val cityName = address?.get(0)?.locality
 
@@ -229,12 +160,5 @@ class ActivityC : AppCompatActivity() {
     private fun firstCheckPermissions() {
         if (hasAccessCoarseLocationPermission() && hasAccessFineLocationPermission())
             hasLocationPermissions = true
-    }
-
-    private fun setCurrentFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fl_fragments, fragment)
-            commit()
-        }
     }
 }
