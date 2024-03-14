44459059c38e9a79115d8bb20a1b6ee5a59811ea
MisKuchum <61450683+MisKuchum@users.noreply.github.com>
2024-03-14 20:26:17 +0300

    L24. Added ViewPager on WorkFragment.
    Добавил ViewPager с 4-мя картинками на WorkFragment.

--- app/src/main/java/com/example/androidtasks/ActivityC.kt
+++ app/src/main/java/com/example/androidtasks/ActivityC.kt
@@ -1,7 +1,5 @@
 package com.example.androidtasks
 
-import android.content.pm.PackageManager
-import android.location.Location
 import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -10,33 +8,17 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Spinner
-import android.widget.TextView
 import android.widget.Button
 import android.widget.EditText
-import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import android.Manifest
-import android.location.Geocoder
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import java.util.Locale
 import android.widget.ImageView
-import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class ActivityC : AppCompatActivity() {
     private val TAG = "MyApp"
-    private var hasLocationPermissions = false
-    private lateinit var etBirthdate: EditText
-    private lateinit var spBenchPress: Spinner
-    private lateinit var etCity: EditText
-    private lateinit var btnGetLocation: Button
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     private lateinit var ivProfilePhoto: ImageView
     private lateinit var bottomNavigationView: BottomNavigationView
 
@@ -45,39 +27,9 @@ class ActivityC : AppCompatActivity() {
         Log.d(TAG, "ActivityC is [onCreate] now")
 
         setContentView(R.layout.activity_c)
-        etBirthdate = findViewById(R.id.et_birthdate)
-        spBenchPress = findViewById(R.id.sp_bench_press)
-        etCity = findViewById(R.id.et_city)
-        btnGetLocation = findViewById(R.id.btn_get_location)
+
         ivProfilePhoto = findViewById(R.id.iv_profile_photo)
         bottomNavigationView = findViewById(R.id.bottomNavigationView)
-
-        val difficultyArray = resources.getStringArray(R.array.difficulty)
-        val spDifficultyAdapter = ArrayAdapter(
-            this,
-            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-            difficultyArray)
-        spBenchPress.adapter = spDifficultyAdapter
-
-        spBenchPress.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                var power = when ((view as TextView).text.toString()) {
-                    "Сложно" -> "Хиленький"
-                    "Средне" -> "Ну норм"
-                    "Легко" -> "Перебор"
-                    else -> "Инопланетянин какой-то"
-                }
-
-                Toast.makeText(this@ActivityC, power, Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-        }
 
         ivProfilePhoto.setOnClickListener {
             Intent(Intent.ACTION_GET_CONTENT).also {
@@ -86,33 +38,16 @@ class ActivityC : AppCompatActivity() {
             }
         }
 
-        firstCheckPermissions()
-
-        val setLocationDialog = AlertDialog.Builder(this)
-            .setTitle("Установка местоположения")
-            .setMessage("Вы хотите установить местоположение автоматически?")
-            .setIcon(R.drawable.ic_add_location)
-            .setPositiveButton("Да") { _, _ ->
-                setLocation()
-            }
-            .setNegativeButton("Нет") {_, _ ->
-                Toast.makeText(this, "Тогда зачем кнопку нажимал?", Toast.LENGTH_LONG).show()
-            }
-            .create()
-
-        setCurrentFragment(ProfileFragment())
         bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId) {
-                R.id.mi_profile  -> setCurrentFragment(ProfileFragment())
-                R.id.mi_work -> setCurrentFragment(WorkFragment())
-                R.id.mi_hobby -> setCurrentFragment(HobbyFragment())
+                R.id.mi_profile  -> changeFragment(FragmentTag.PROFILE)
+                R.id.mi_work -> changeFragment(FragmentTag.WORK)
+                R.id.mi_hobby -> changeFragment(FragmentTag.HOBBY)
             }
             true
         }
 
-        btnGetLocation.setOnClickListener {
-            setLocationDialog.show()
-        }
+        changeFragment(FragmentTag.PROFILE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -154,87 +89,34 @@ class ActivityC : AppCompatActivity() {
         Log.d(TAG, "ActivityC is [onDestroy] now")
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == 0 &&
-            grantResults.isNotEmpty()) {
-            if (grantResults.filter { result -> result == PackageManager.PERMISSION_GRANTED }.size == permissions.size) {
-                hasLocationPermissions = true
-                setLocation()
-            }
-            else
-                Toast.makeText(applicationContext, "Нужно разрешение", Toast.LENGTH_LONG)
-                    .show()
-        }
-    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean = MyOptionsMenu().create(this, menu)
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean = MyOptionsMenu().itemSelected(this, item)
 
     fun onClickInputButton(view: View) {
-        intent.putExtra("input", etBirthdate.text.toString())
         setResult(RESULT_OK, intent)
         finish()
     }
 
-    private fun hasAccessCoarseLocationPermission() =
-        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    private fun changeFragment(fragmentTag: FragmentTag) {
+        val transaction = supportFragmentManager.beginTransaction()
 
-    private fun hasAccessFineLocationPermission() =
-        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        val currentFragment = supportFragmentManager.primaryNavigationFragment
+        if (currentFragment != null)
+            transaction.hide(currentFragment)
 
-    private fun requestPermissions() {
-        var permissionsToRequest = mutableListOf<String>()
+        var tempFragment = supportFragmentManager.findFragmentByTag(fragmentTag.toString())
+        if (tempFragment == null) {
+            tempFragment = when (fragmentTag) {
+                FragmentTag.PROFILE -> ProfileFragment()
+                FragmentTag.WORK -> WorkFragment()
+                FragmentTag.HOBBY -> HobbyFragment()
+            }
+            transaction.add(R.id.fl_fragments, tempFragment, fragmentTag.toString())
+        } else
+            transaction.show(tempFragment)
 
-        if (!hasAccessCoarseLocationPermission())
-            permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-        if (!hasAccessFineLocationPermission())
-            permissionsToRequest.add(Manifest.permission.ACCESS_FINE_LOCATION)
-
-        Log.d(TAG, "${permissionsToRequest.toString()}")
-
-        if (permissionsToRequest.isNotEmpty())
-            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), 0)
-        else
-            hasLocationPermissions = true
-    }
-
-    private fun setLocation() {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-        if (hasLocationPermissions) {
-            fusedLocationClient.lastLocation
-                .addOnSuccessListener { location : Location? ->
-                    if (location != null) {
-                        val geoCoder = Geocoder(this, Locale.getDefault())
-                        val address = geoCoder.getFromLocation(location.latitude,location.longitude,1)
-                        val cityName = address?.get(0)?.locality
-
-                        if (cityName != null) etCity.setText(cityName)
-                        else etCity.setText("Нет информации")
-                    }
-                }
-                .addOnFailureListener { e ->
-                    Log.d(TAG, "Не удалось получить информацию о местоположении!")
-                }
-        }
-        else requestPermissions()
-    }
-
-    private fun firstCheckPermissions() {
-        if (hasAccessCoarseLocationPermission() && hasAccessFineLocationPermission())
-            hasLocationPermissions = true
-    }
-
-    private fun setCurrentFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fl_fragments, fragment)
-            commit()
-        }
+        transaction.setPrimaryNavigationFragment(tempFragment)
+        transaction.commit()
     }
 }
--- app/src/main/java/com/example/androidtasks/WorkFragment.kt
+++ app/src/main/java/com/example/androidtasks/WorkFragment.kt
@@ -5,12 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 
 class WorkFragment : Fragment() {
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_work, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewPager = view.findViewById(R.id.view_pager)
+
+        val images = listOf(
+            R.drawable.work,
+            R.drawable.work2,
+            R.drawable.work3,
+            R.drawable.work4
+        )
+        val adapter = ViewPagerAdapter(images)
+        viewPager.adapter = adapter
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            WorkFragment().apply { }
     }
 }
