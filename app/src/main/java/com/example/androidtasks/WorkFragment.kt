4eb2885bc6aef86e6cc233154d37ad85e8db8168
MisKuchum <61450683+MisKuchum@users.noreply.github.com>
2024-03-14 20:40:01 +0300

    L25. Added TabLayout to ViewPager on WorkFragment.
    Добавил TabLayout для ViewPager на WorkFragment. При показе каждой картинки выводится свой Toast.

--- app/src/main/java/com/example/androidtasks/WorkFragment.kt
+++ app/src/main/java/com/example/androidtasks/WorkFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class WorkFragment : Fragment() {
     private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +24,7 @@ class WorkFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewPager = view.findViewById(R.id.view_pager)
+        tabLayout = view.findViewById(R.id.tab_layout)
 
         val images = listOf(
             R.drawable.work,
@@ -29,6 +34,34 @@ class WorkFragment : Fragment() {
         )
         val adapter = ViewPagerAdapter(images)
         viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (images[position]) {
+                R.drawable.work -> tab.text = getString(R.string.wolf)
+                R.drawable.work2 -> tab.text = getString(R.string.money)
+                R.drawable.work3 -> tab.text = getString(R.string.home)
+                R.drawable.work4 -> tab.text = getString(R.string.experience)
+            }
+        }.attach()
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val toastMessage = when (tab?.text) {
+                    getString(R.string.wolf) -> getString(R.string.not_wolf)
+                    getString(R.string.money) -> getString(R.string.walk)
+                    getString(R.string.home) -> getString(R.string.not_work)
+                    getString(R.string.experience) -> getString(R.string.not_experience)
+                    else -> ""
+                }
+                Toast.makeText(requireContext(), toastMessage, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                onTabSelected(tab)
+            }
+        })
     }
 
     companion object {
